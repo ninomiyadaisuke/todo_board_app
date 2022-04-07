f12e9d898b028bf8f92cbd131c1c1181baa790ae
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2022-04-07 11:31:32 +0900

    create InputForm

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import * as color from './color'
 import { Card } from './Card'
 import { PlusIcon } from './icon'
+import { InputForm as _InputForm } from './InputForm'
 
 export const Column = ({
   title,
@@ -16,6 +17,11 @@ export const Column = ({
   }[]
 }) => {
   const totalCount = cards.length
+  const [text, setText] = useState('')
+  const [inputMode, setInputMode] = useState(false)
+  const toggleInput = () => setInputMode((v) => !v)
+  const confirmInput = () => setText('')
+  const cancelInput = () => setInputMode(false)
 
   return (
     <Container>
@@ -23,9 +29,16 @@ export const Column = ({
         <CountBadge>{totalCount}</CountBadge>
         <ColumnName>{title}</ColumnName>
 
-        <AddButton />
+        <AddButton onClick={toggleInput} />
       </Header>
-
+      {inputMode && (
+        <InputForm
+          value={text}
+          onChange={setText}
+          onConfirm={confirmInput}
+          onCancel={cancelInput}
+        />
+      )}
       <VerticalScroll>
         {cards.map(({ id, text }) => (
           <Card key={id} text={text} />
@@ -82,6 +95,10 @@ const AddButton = styled.button.attrs({
   :hover {
     color: ${color.Blue};
   }
+`
+
+const InputForm = styled(_InputForm)`
+  padding: 8px;
 `
 
 const VerticalScroll = styled.div`
